4f1e831bef2adc92390407c75273213980cf4b84
gealsanchez <gealsanchez@gmail.com>
2023-04-18 17:18:24 -0500

    Decorate a class

---
+++ main.rb
@@ -0,0 +1,10 @@
+require_relative './person'
+require_relative './capitalize_decorator'
+require_relative './trimmer_decorator'
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalizedPerson = CapitalizeDecorator.new(person)
+capitalizedPerson.correct_name
+capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+capitalizedTrimmedPerson.correct_name
