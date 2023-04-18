7f6d562035eaa64be348efdb78bc6fa365c27eee
gealsanchez <gealsanchez@gmail.com>
2023-04-18 17:25:56 -0500

    Rubocop report changes

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ require_relative './trimmer_decorator'
 
 person = Person.new(22, 'maximilianus')
 person.correct_name
-capitalizedPerson = CapitalizeDecorator.new(person)
-capitalizedPerson.correct_name
-capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-capitalizedTrimmedPerson.correct_name
+capitalized_Person = CapitalizeDecorator.new(person)
+capitalized_Person.correct_name
+capitalized_Trimmed_Person = TrimmerDecorator.new(capitalized_Person)
+capitalized_Trimmed_Person.correct_name
